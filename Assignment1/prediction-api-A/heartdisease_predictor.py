2a9476bf4914411805b3f728e3322fc88043ff6f
TingWu28 <149304922+TingWu28@users.noreply.github.com>
2024-10-23 15:51:43 +0200

    Update heartdisease_predictor.py

--- Assignment1/prediction-api-A/heartdisease_predictor.py
+++ Assignment1/prediction-api-A/heartdisease_predictor.py
@@ -1,16 +1,20 @@
+
 import json
+import logging
 import os
+import pickle
+from io import StringIO
 
 import pandas as pd
 from flask import jsonify
-from keras.models import load_model
-import logging
-from io import StringIO
 
 
 class HeartdiseasePredictor:
     def __init__(self):
         self.model = None
+
+    def load_model(self, file_path):
+        self.model = pickle.load(open(file_path, 'rb'))
 
     def predict_single_record(self, prediction_input):
         logging.debug(prediction_input)
@@ -18,15 +22,17 @@ class HeartdiseasePredictor:
             try:
                 model_repo = os.environ['MODEL_REPO']
                 file_path = os.path.join(model_repo, "best_model.pkl")
-                self.model = load_model(file_path)
+                self.model = pickle.load(open(file_path, 'rb'))
             except KeyError:
                 print("MODEL_REPO is undefined")
-                self.model = load_model('best_model.pkl')
+                self.model = pickle.load(open("best_model.pkl", 'rb'))
 
         df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
-        y_pred = self.model.predict(df)
-        logging.info(y_pred[0])
-        status = (y_pred[0] > 0.5)
-        logging.info(type(status[0]))
+        xNew = df[['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach','exang','oldpeak','slope','ca','thal']]
+        dfcp = df.copy()
+        y_classes = self.model.predict(xNew)
+        logging.info(y_classes)
+        dfcp['pclass'] = y_classes.tolist()
+        status = (dfcp['pclass'][0] > 0.5)
         # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
-        return jsonify({'result': str(status[0])}), 200
+        return str(status)
