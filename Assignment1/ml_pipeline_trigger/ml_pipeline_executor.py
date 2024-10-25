6e63a6d758b6dff5ca7a963e4417e4aa1cf62ad3
TingWu28 <149304922+TingWu28@users.noreply.github.com>
2024-10-25 14:43:50 +0200

    Update ml_pipeline_executor.py

--- Assignment1/ml_pipeline_trigger/ml_pipeline_executor.py
+++ Assignment1/ml_pipeline_trigger/ml_pipeline_executor.py
@@ -3,24 +3,27 @@ import logging
 import os
 
 def run_ml_pipeline():
+    # Define the GCS path to your pipeline YAML and pipeline root
     pipeline_def = "gs://temp_de2024_2117913/heartdisease_predictor_training_pipeline.yaml"
     pipeline_root = "gs://temp_de2024_2117913"
 
+    # Define the parameters that the pipeline requires
     parameter_values = {
         'project_id': 'de2024-435020',
         'data_bucket': 'data_de2024_2117913',
         'dataset_uri': 'gs://data_de2024_2117913/datasets/training_set.csv',
-        'model_repo': 'model_de2024_2117913',
-        'trigger_id': '72f313bc-6634-4db7-bd61-f97545795c8f'
+        'model_repo': 'models_de2024_2117913'
     }
 
+    # Create the job
     job = aip.PipelineJob(
         display_name="retrain-model",
         template_path=pipeline_def,
         pipeline_root=pipeline_root,
         parameter_values=parameter_values
     )
-    
+
+    # Run the job
     job.run()
 
 if __name__ == "__main__":
