da5dbe8715807af80836e7dcd12608ce301c5960
TingWu28 <149304922+TingWu28@users.noreply.github.com>
2024-10-27 23:12:37 +0100

    Update ml_pipeline_executor.py

--- Assignment1/ml_pipeline_trigger/ml_pipeline_executor.py
+++ Assignment1/ml_pipeline_trigger/ml_pipeline_executor.py
@@ -25,7 +25,7 @@ def run_ml_pipeline():
     )
 
     # Run the job
-    job.run()
+    job.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
